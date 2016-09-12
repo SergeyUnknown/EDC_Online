7d9d1bdc46d6fdca9475de2729e6f08afe0da79e
SergeyUnknown <sergeyi.unknown@gmail.com>
2016-09-12 13:34:16 +0400

    Начало ролей

--- EDC/Site.Master.designer.cs
+++ EDC/Site.Master.designer.cs
@@ -31,6 +31,15 @@ namespace EDC {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder FeaturedContent;
         
         /// <summary>
+        /// liAdmin элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl liAdmin;
+        
+        /// <summary>
         /// MainContent элемент управления.
         /// </summary>
         /// <remarks>
